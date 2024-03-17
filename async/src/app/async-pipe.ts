18863f7a913e56f72fa86ea3ce978c1377ef3974
Oren Karaso <orenkaraso@gmail.com>
2024-03-17 22:15:27 +0200

    Removed unnecessary type assertion, variable rename and added a comment

--- async/src/app/async-pipe.ts
+++ async/src/app/async-pipe.ts
@@ -2,7 +2,7 @@ import { ChangeDetectorRef, OnDestroy, Pipe, PipeTransform } from "@angular/core
 import { Observable, Subject, from, isObservable } from "rxjs";
 import { takeUntil } from "rxjs/operators";
 
-@Pipe({ name: 'myAsync', pure: false })
+@Pipe({ name: 'myAsync', pure: false }) // Marking an unpure so the pipe will be triggered on every change detection cycle
 export class MyAsyncPipe implements PipeTransform, OnDestroy {
 
     private _currentValue?: any;
@@ -15,8 +15,8 @@ export class MyAsyncPipe implements PipeTransform, OnDestroy {
         this._destroy$.complete();
     }
 
-    transform<T>(input$: Observable<T> | Promise<T>): T | undefined {
-        const data$ = isObservable(input$) ? input$ : from(input$) as Observable<T>; // Converting to observable in case of a promise input
+    transform<T>(asyncDataSource$: Observable<T> | Promise<T>): T | undefined {
+        const data$ = isObservable(asyncDataSource$) ? asyncDataSource$ : from(asyncDataSource$); // Converting to observable in case of a promise input
         data$.pipe(
             takeUntil(this._destroy$) // onDestroy will next the subject and complete the data$ stream
         ).subscribe({
